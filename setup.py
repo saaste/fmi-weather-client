7933db5d0dcf25c89c564bde70b19cea75dc6b3a
Mika Hiltunen <mika@hiltunen.info>
2020-02-25 23:08:01 +0200

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fmi-weather-client",
-    version="0.0.7",
+    version="0.0.8",
     author="Mika Hiltunen",
     author_email="saaste@gmail.com",
     description="Library for fetching weather information from Finnish Meteorological Institute (FMI)",
