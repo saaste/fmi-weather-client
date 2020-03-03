083a487f7efa91cad4e672e5b1648d18106ae031
Mika Hiltunen <mika@hiltunen.info>
2020-03-03 20:21:22 +0200

    Add async support (#23)

--- fmi_weather_client/__init__.py
+++ fmi_weather_client/__init__.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from fmi_weather_client import http
 from fmi_weather_client.models import Weather
 from fmi_weather_client.parsers import forecast as forecast_parser
@@ -11,7 +13,22 @@ def weather_by_coordinates(lat: float, lon: float) -> Weather:
     :param lon: Longitude (e.g. 62.39758)
     :return: Latest weather information
     """
-    response = http.request_observations_by_coordinates(lat, lon)
+    response = http.request_weather_by_coordinates(lat, lon)
+    forecast = forecast_parser.parse_forecast(response)
+    weather_state = forecast.forecasts[-1]
+    return Weather(forecast.place, forecast.lat, forecast.lon, weather_state)
+
+
+async def async_weather_by_coordinates(lat: float, lon: float) -> Weather:
+    """
+    Get the latest weather information by coordinates asynchronously.
+
+    :param lat: Latitude (e.g. 25.67087)
+    :param lon: Longitude (e.g. 62.39758)
+    :return: Latest weather information
+    """
+    loop = asyncio.get_event_loop()
+    response = await loop.run_in_executor(None, http.request_weather_by_coordinates, lat, lon)
     forecast = forecast_parser.parse_forecast(response)
     weather_state = forecast.forecasts[-1]
     return Weather(forecast.place, forecast.lat, forecast.lon, weather_state)
@@ -24,7 +41,21 @@ def weather_by_place_name(name: str) -> Weather:
     :param name: Place name (e.g. Kaisaniemi, Helsinki)
     :return: Latest weather information
     """
-    response = http.request_observations_by_place(name)
+    response = http.request_weather_by_place(name)
+    forecast = forecast_parser.parse_forecast(response)
+    weather_state = forecast.forecasts[-1]
+    return Weather(forecast.place, forecast.lat, forecast.lon, weather_state)
+
+
+async def async_weather_by_place_name(name: str) -> Weather:
+    """
+    Get the latest weather information by place name asynchronously.
+
+    :param name: Place name (e.g. Kaisaniemi, Helsinki)
+    :return: Latest weather information
+    """
+    loop = asyncio.get_event_loop()
+    response = await loop.run_in_executor(None, http.request_weather_by_place, name)
     forecast = forecast_parser.parse_forecast(response)
     weather_state = forecast.forecasts[-1]
     return Weather(forecast.place, forecast.lat, forecast.lon, weather_state)
@@ -32,12 +63,24 @@ def weather_by_place_name(name: str) -> Weather:
 
 def forecast_by_place_name(name: str, timestep_hours: int = 24):
     """
-    Get the latest forecast by place name
+    Get the latest forecast by place name.
     :param name: Place name
     :param timestep_hours: Hours between forecasts
     :return: Latest forecast
     """
     response = http.request_forecast_by_place(name, timestep_hours)
+    return forecast_parser.parse_forecast(response)
+
+
+async def async_forecast_by_place_name(name: str, timestep_hours: int = 24):
+    """
+    Get the latest forecast by place name asynchronously.
+    :param name: Place name
+    :param timestep_hours: Hours between forecasts
+    :return: Latest forecast
+    """
+    loop = asyncio.get_event_loop()
+    response = await loop.run_in_executor(None, http.request_forecast_by_place, name, timestep_hours)
     return forecast_parser.parse_forecast(response)
 
 
@@ -50,4 +93,17 @@ def forecast_by_coordinates(lat: float, lon: float, timestep_hours: int = 24):
     :return: Latest forecast
     """
     response = http.request_forecast_by_coordinates(lat, lon, timestep_hours)
+    return forecast_parser.parse_forecast(response)
+
+
+async def async_forecast_by_coordinates(lat: float, lon: float, timestep_hours: int = 24):
+    """
+    Get the latest forecast by coordinates
+    :param lat: Latitude (e.g. 25.67087)
+    :param lon: Longitude (e.g. 62.39758)
+    :param timestep_hours: Hours between forecasts
+    :return: Latest forecast
+    """
+    loop = asyncio.get_event_loop()
+    response = await loop.run_in_executor(None, http.request_forecast_by_coordinates, lat, lon, timestep_hours)
     return forecast_parser.parse_forecast(response)
--- fmi_weather_client/test/test_fmi_weather.py
+++ fmi_weather_client/test/test_fmi_weather.py
@@ -1,6 +1,8 @@
 import unittest
 from unittest import mock
 
+import asyncio
+
 import fmi_weather_client
 import fmi_weather_client.test.test_data as test_data
 from fmi_weather_client.errors import NoDataAvailableError, ServiceError
@@ -9,20 +11,26 @@ from fmi_weather_client.errors import NoDataAvailableError, ServiceError
 class FMIWeatherTest(unittest.TestCase):
 
     @mock.patch('requests.get', side_effect=test_data.mock_place_forecast_response)
-    def test_get_observation_by_place(self, mock_get):
-        observation = fmi_weather_client.weather_by_place_name('Iisalmi')
-        self.assertEqual(observation.place, 'Iisalmi')
-        self.assertEqual(observation.data.time.timestamp(), 1582027200.0)
-        self.assertEqual(observation.data.temperature.value, 2.34)
-        self.assertEqual(observation.data.wind_speed.value, 3.52)
+    def test_get_weather_by_place(self, mock_get):
+        weather = fmi_weather_client.weather_by_place_name('Iisalmi')
+        self.assert_name_weather(weather)
+
+    @mock.patch('requests.get', side_effect=test_data.mock_place_forecast_response)
+    def test_async_get_weather_by_place(self, mock_get):
+        loop = asyncio.get_event_loop()
+        weather = loop.run_until_complete(fmi_weather_client.async_weather_by_place_name('Iisalmi'))
+        self.assert_name_weather(weather)
 
     @mock.patch('requests.get', side_effect=test_data.mock_coordinate_forecast_response)
-    def test_get_observation_by_coordinates(self, mock_get):
-        observation = fmi_weather_client.weather_by_coordinates(63.14343, 27.31317)
-        self.assertEqual(observation.place, 'Sauoiva')
-        self.assertEqual(observation.data.time.timestamp(), 1582027200.0)
-        self.assertEqual(observation.data.temperature.value, 0.37)
-        self.assertEqual(observation.data.wind_speed.value, 1.35)
+    def test_get_weather_by_coordinates(self, mock_get):
+        weather = fmi_weather_client.weather_by_coordinates(63.14343, 27.31317)
+        self.assert_coordinate_weather(weather)
+
+    @mock.patch('requests.get', side_effect=test_data.mock_coordinate_forecast_response)
+    def test_async_get_weather_by_coordinates(self, mock_get):
+        loop = asyncio.get_event_loop()
+        weather = loop.run_until_complete(fmi_weather_client.async_weather_by_coordinates(63.14343, 27.31317))
+        self.assert_coordinate_weather(weather)
 
     @mock.patch('requests.get', side_effect=test_data.mock_empty_response)
     def test_empty_response(self, mock_get):
@@ -47,6 +55,38 @@ class FMIWeatherTest(unittest.TestCase):
     @mock.patch('requests.get', side_effect=test_data.mock_place_forecast_response)
     def test_get_forecast_by_place_name(self, mock_get):
         forecast = fmi_weather_client.forecast_by_place_name('Iisalmi')
+        self.assert_name_forecast(forecast)
+
+    @mock.patch('requests.get', side_effect=test_data.mock_place_forecast_response)
+    def test_async_get_forecast_by_place_name(self, mock_get):
+        loop = asyncio.get_event_loop()
+        forecast = loop.run_until_complete(fmi_weather_client.async_forecast_by_place_name('Iisalmi'))
+        self.assert_name_forecast(forecast)
+
+    @mock.patch('requests.get', side_effect=test_data.mock_coordinate_forecast_response)
+    def test_get_forecast_by_coordinates(self, mock_get):
+        forecast = fmi_weather_client.forecast_by_coordinates(29.742731, 67.583988)
+        self.assert_coordinate_forecast(forecast)
+
+    @mock.patch('requests.get', side_effect=test_data.mock_coordinate_forecast_response)
+    def test_async_get_forecast_by_coordinates(self, mock_get):
+        loop = asyncio.get_event_loop()
+        forecast = loop.run_until_complete(fmi_weather_client.async_forecast_by_coordinates(29.742731, 67.583988))
+        self.assert_coordinate_forecast(forecast)
+
+    def assert_name_weather(self, weather):
+        self.assertEqual(weather.place, 'Iisalmi')
+        self.assertEqual(weather.data.time.timestamp(), 1582027200.0)
+        self.assertEqual(weather.data.temperature.value, 2.34)
+        self.assertEqual(weather.data.wind_speed.value, 3.52)
+
+    def assert_coordinate_weather(self, weather):
+        self.assertEqual(weather.place, 'Sauoiva')
+        self.assertEqual(weather.data.time.timestamp(), 1582027200.0)
+        self.assertEqual(weather.data.temperature.value, 0.37)
+        self.assertEqual(weather.data.wind_speed.value, 1.35)
+
+    def assert_name_forecast(self, forecast):
         self.assertEqual(forecast.place, 'Iisalmi')
         self.assertEqual(forecast.lat, 27.19067)
         self.assertEqual(forecast.lon, 63.55915)
@@ -55,9 +95,7 @@ class FMIWeatherTest(unittest.TestCase):
         self.assertEqual(forecast.forecasts[1].pressure.value, 974.67)
         self.assertEqual(forecast.forecasts[4].humidity.value, 96.31)
 
-    @mock.patch('requests.get', side_effect=test_data.mock_coordinate_forecast_response)
-    def test_get_forecast_by_coordinates(self, mock_get):
-        forecast = fmi_weather_client.forecast_by_coordinates(29.742731, 67.583988)
+    def assert_coordinate_forecast(self, forecast):
         self.assertEqual(forecast.place, 'Sauoiva')
         self.assertEqual(forecast.lat, 29.74273)
         self.assertEqual(forecast.lon, 67.58399)
