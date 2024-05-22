024955697ef066850562eea2f703ab68555073fe
Mika Hiltunen <github@haaska.net>
2024-05-22 21:42:08 +0300

    Increase version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fmi-weather-client",
-    version="0.2.2",
+    version="0.3.0",
     author="Mika Hiltunen",
     author_email="saaste@gmail.com",
     description="Library for fetching weather information from Finnish Meteorological Institute (FMI)",
@@ -14,14 +14,17 @@ setuptools.setup(
     url="https://github.com/saaste/fmi-weather-client",
     packages=setuptools.find_packages(exclude=["*test", "*test.*"]),
     install_requires=[
-        'requests>=2.31.0',
+        'requests>=2.32.2',
         'xmltodict>=0.13.0'
     ],
     classifiers=[
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7.17',
+    python_requires='>=3.8.19',
 )
