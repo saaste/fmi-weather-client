8fe129bb6072277aaa0fad7f0464cadf24d893e3
Mika H <saaste@users.noreply.github.com>
2024-10-14 16:47:14 +0300

    Update version (#62)
    * Update requests
    
    * Drop python 3.8. Update versions.
    
    * Update github action
    
    * Update README

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fmi-weather-client",
-    version="0.3.0",
+    version="0.4.0",
     author="Mika Hiltunen",
     author_email="saaste@gmail.com",
     description="Library for fetching weather information from Finnish Meteorological Institute (FMI)",
@@ -14,17 +14,17 @@ setuptools.setup(
     url="https://github.com/saaste/fmi-weather-client",
     packages=setuptools.find_packages(exclude=["*test", "*test.*"]),
     install_requires=[
-        'requests>=2.32.2',
-        'xmltodict>=0.13.0'
+        'requests>=2.32.3',
+        'xmltodict>=0.14.1'
     ],
     classifiers=[
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8.19',
+    python_requires='>=3.9.20',
 )
